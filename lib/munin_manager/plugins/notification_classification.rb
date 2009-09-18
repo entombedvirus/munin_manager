117f6e9e09eacf148b24a697ad400c3c8f54bf4f
Chris Vincent <chris@seriousbusiness.com>
2009-09-18 12:24:47 -0700

    added notification classification plugin for monitoring the rate of notification classification creation (new NCs / hour)

---
+++ lib/munin_manager/plugins/notification_classification.rb
@@ -0,0 +1,27 @@
+require 'rubygems'
+require 'active_support'
+
+module MuninManager
+  class Plugins::NotificationClassification
+    include ActsAsMuninPlugin
+    
+    def config
+      "graph_title Notification Classification Creation
+      graph_vlabel new classifications / hour
+      notification_rate.label new classifications / hour
+      notification_rate.type derive
+      notification_rate.warning 10
+      notification_rate.critical 100"
+    end
+    
+    def self.run
+      allowed_commands = ['config']
+      if cmd = ARGV[0] and allowed_commands.include? cmd
+        puts new.config
+      else
+        cmd = "/opt/local/bin/mysql5 -u #{ENV['mysql_user']} --password=#{ENV['mysql_password']} -h #{ENV['host']} -e 'use #{ENV['database']}; select count(*) from notification_classifications where created_at >= \"#{1.hour.ago.to_s(:db)}\";' --skip-column-names --silent"
+        puts "notification_rate.value %s" % `#{cmd}`
+      end
+    end
+  end
+end
